67b8990446499c917521e8edd3772cc3ad1966eb
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-08-29 19:31:53 -0300

    exclui arquivos desnecessários

--- __tests__/entities/matches/index.js
+++
@@ -1,9 +0,0 @@
-const allMatches = require('./allMatches');
-const onlyInProgress = require('./onlyInProgress');
-const onlyFinished = require('./onlyFinished');
-
-module.exports = {
-  allMatches,
-  onlyInProgress,
-  onlyFinished,
-};
