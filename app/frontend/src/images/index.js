669dfb1f9a77e71ef2033fb9d3d6d8553683b9d2
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-11-14 19:22:21 -0300

    feat: altera a logo

--- app/frontend/src/images/index.js
+++ app/frontend/src/images/index.js
@@ -1,5 +1,5 @@
 export { default as exitToAppImg } from './exit_to_app.png';
-export { default as negativeLogo } from './negative_logo.png';
+export { default as negativeLogo } from './negative_logo_n.png';
 export { default as check } from './check.png';
 export { default as editIcon } from './pencil.png';
-export { default as positiveLogo } from './positive_logo.png';
+export { default as positiveLogo } from './positive_logo_n.png';
