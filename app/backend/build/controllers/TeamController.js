7ce1cbc54ca6efbf9ae6ba5a70bea7e6ccdc2d45
Jhony Altoé <jhonymikealtoe@hotmail.com>
2023-05-27 22:51:02 -0300

    feat: instal and config postgres for deploy

--- app/backend/build/controllers/TeamController.js
+++ app/backend/build/controllers/TeamController.js
@@ -5,8 +5,8 @@ class TeamController {
     constructor(team) {
         this.team = team;
         this.getAll = (0, utils_1.controllerWrapper)(async (_req, res) => {
-            const teams = await this.team.getAll();
-            res.status(200).json(teams);
+            // const teams = await this.team.getAll();
+            res.status(200).json('teams');
         });
         this.getOne = (0, utils_1.controllerWrapper)(async (req, res) => {
             const team = await this.team.getOne(Number(req.params.id));
--- app/backend/build/database/config/database.js
+++ app/backend/build/database/config/database.js
@@ -2,14 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const config = {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '123456',
-    database: 'TRYBE_FUTEBOL_CLUBE',
-    host: process.env.DB_HOST || 'localhost',
+    username: process.env.POSTGRES_USER || 'root',
+    password: process.env.POSTGRES_PASSWORD || '123456',
+    database: process.env.POSTGRES_DATABASE || 'TRYBE_FUTEBOL_CLUBE',
+    host: process.env.POSTGRES_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 3002,
-    dialect: 'mysql',
+    dialect: 'postgres',
     dialectOptions: {
         timezone: 'Z',
+        ssl: true,
     },
     logging: false,
 };
